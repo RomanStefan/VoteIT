0adb5b1203187344eee5a03c0c76ce42932d2973
Roman Stefan <roman.stefanel@gmail.com>
2019-06-13 14:41:51 +0300

    Added Percentages for piechart

--- VoteIT/VoteIT/ClientApp/src/components/SecondComponents/ChartPie.js
+++ VoteIT/VoteIT/ClientApp/src/components/SecondComponents/ChartPie.js
@@ -9,13 +9,13 @@ export class ChartPie extends Component {
         super(props);
         this.state = {
             voteResults: [],
-            candidates: []
+            candidates: [],
+            percentages: []
         };
 
 
         axios.get('https://localhost:44319/VotingHistory/GetTheResults').then(res => {
             this.setState({ voteResults: res.data });
-            console.log(res.data);
             var candidates = [];
             for (var i = 0; i < res.data.length; i++) {
 
@@ -32,7 +32,7 @@ export class ChartPie extends Component {
                         candidate.color = "#ff4000";
                         break;
                     case 'PNL':
-                        candidate.color = "#ffff00";
+                        candidate.color = "#ffdb4d";
                         break;
                     case 'USR':
                         candidate.color = "#00ffff";
@@ -41,18 +41,70 @@ export class ChartPie extends Component {
                 candidates.push(candidate)
             }
             this.setState({ candidates: candidates });
-            console.log(candidates);
+
+            //calculam numarul total de voturi
+            var totalVotes = 0; 
+            for (var i = 0; i < res.data.length; i++) {
+                totalVotes += res.data[i].numberOfVotes;
+            }
+
+            var percentages = [];
+            for (var i = 0; i < res.data.length; i++) {
+
+                var percentage = {
+                    candidateName: "",
+                    value: 0,
+                    politicalParty: "",
+                    color: ""
+                };
+                percentage.candidateName = res.data[i].candidateName;
+                percentage.value = ((100 * res.data[i].numberOfVotes) / totalVotes).toFixed(2);
+                percentage.politicalParty = res.data[i].politicalParty;
+
+                switch (res.data[i].politicalParty) {
+                    case 'PSD':
+                        percentage.color = "#ff4000";
+                        break;
+                    case 'PNL':
+                        percentage.color = "#ffdb4d";
+                        break;
+                    case 'USR':
+                        percentage.color = "#00ffff";
+                        break;
+                }
+                percentages.push(percentage);
+            }
+            this.setState({ percentages: percentages });
+            console.log(percentages);
         });
     }
 
+    displayPercentages(){
+        return this.state.percentages.map((percentage, index) => {
+            const { candidateName, value, color, politicalParty } = percentage //destructuring
+            return (
+                <div>
+                    <p>{candidateName} - <font color={color}><b>{politicalParty}</b></font>  - <b>{value}</b>%</p>
+                </div>
+               )
+        });
+}
+
+
     render() {
         return (
-            <div>
-                <PieChart className="chart"
-                    data={
-                        this.state.candidates
-                    }
-                />
+            <div id="chartContainer">
+                    <p>The results are: </p>        
+                <div id = "row">
+                    <PieChart className="chart"
+                        data={
+                            this.state.candidates
+                        }
+                    />
+                </div>               
+                <div id= "rowResults">
+                    {this.displayPercentages()}
+                </div>               
             </div>
         );
     }
